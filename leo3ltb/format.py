9fdf9e531a55da666c328fe748d1f1aa726b61d0
Marco Treger <marco.traeger@googlemail.com>
2019-07-26 17:47:16 +0200

    Format: removed color formats (temp).

--- leo3ltb/format.py
+++ leo3ltb/format.py
@@ -1,3 +1,4 @@
+'''
 from colorama import init
 from colorama import Fore, Back, Style
 init()
@@ -18,6 +19,27 @@ def cyan(v):
     return Fore.CYAN + str(v) + Style.RESET_ALL
 def white(v):
     return Fore.WHITE + str(v) + Style.RESET_ALL
+
+def indent(xs, ind):
+    return ind + ('\n'+ind).join(map(str,xs))
+'''
+
+def black(v):
+    return str(v)
+def red(v):
+    return str(v)
+def green(v):
+    return str(v)
+def yellow(v):
+    return str(v)
+def blue(v):
+    return str(v)
+def magenta(v):
+    return str(v)
+def cyan(v):
+    return str(v)
+def white(v):
+    return str(v)
 
 def indent(xs, ind):
     return ind + ('\n'+ind).join(map(str,xs))
